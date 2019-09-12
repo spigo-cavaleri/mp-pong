d4e77b04907cff0b2d57944a01a0d7874dad2458
Kahpya <nikihap@gmail.com>
2019-09-12 13:45:13 +0200

    Create GameObject Class with bacis funtionality

---
+++ PongGame/GameObject.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PongGame
+{
+    public abstract class GameObject
+    {
+        public Vector2 Position;
+        public Texture2D Sprite;
+
+        public GameObject(Texture2D sprite, Vector2 position)
+        {
+            Position = position;
+
+            Sprite = sprite;
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Sprite, Position, Color.White);
+        }
+    }
+}
