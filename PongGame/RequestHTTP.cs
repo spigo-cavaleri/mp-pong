09125e33c978a41af0c49bc36e09f60642ddc220
Bjarke <bjarkemm@live.dk>
2019-09-13 10:44:32 +0200

    Skelet til opbygningen af klassen

---
+++ PongGame/RequestHTTP.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace PongGame
+{
+    class RequestHTTP
+    {
+        int paraLul = 0;
+
+        public RequestHTTP()
+        {
+
+        }
+
+        public void DoMagic()
+        {
+            WebRequest request = WebRequest.Create("https://my-json-server.typicode.com/typicode/demo/comments");
+            request.Method = "POST";
+
+            string postData = "This is a test that posts this string to a server";
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = byteArray.Length;
+
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+
+            WebResponse response = request.GetResponse();
+
+            using (dataStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(dataStream);
+                string responseFromServer = reader.ReadToEnd();
+            }
+        }
+    }
+}
