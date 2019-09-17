43c3da3a8ef6ccf7615abe58cdc1a853be6300d6
Bjarke <bjarkemm@live.dk>
2019-09-17 15:37:29 +0200

    2 methods
    That make you able to send and recieve when loggin in and creating an acount

---
+++ PongGame/JSONMessages/ServerConfirmation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace PongGame.JSONMessages
+{
+    [DataContract]class ServerConfirmation
+    {
+        [DataMember] bool serverSuccess;
+
+        public bool ServerSuccess
+        {
+            get => this.serverSuccess;
+        }
+
+        public ServerConfirmation(bool serverSuccess)
+        {
+            this.serverSuccess = serverSuccess;
+        }
+    }
+}
--- PongGame/RequestHTTP.cs
+++ PongGame/RequestHTTP.cs
@@ -5,21 +5,97 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Net;
+using System.Runtime.Serialization.Json;
 
 namespace PongGame
 {
     class RequestHTTP
-    {
-        int paraLul = 0;
-
+    {   
         public RequestHTTP()
         {
-
+            
         }
 
+        public LoginResponseMessage CreateAccount(string name, string password)
+        {
+            WebRequest request = WebRequest.Create("http//localhost/api/createuser");
+            request.Credentials = CredentialCache.DefaultCredentials;
+            request.Method = "POST";
+            
+            UserInformationMessage uIF = new UserInformationMessage();
+
+            uIF.Username = name;
+            uIF.Password = password;
+
+            MemoryStream mStream = new MemoryStream();
+            DataContractJsonSerializer dJsonSerializer = new DataContractJsonSerializer(typeof(UserInformationMessage));
+
+            dJsonSerializer.WriteObject(mStream, uIF);
+
+            mStream.Position = 0;
+            StreamReader sReader = new StreamReader(mStream);
+
+            string requestString = sReader.ReadToEnd();
+            byte[] msgToSend = System.Text.Encoding.ASCII.GetBytes(requestString);
+            request.ContentLength = msgToSend.Length;
+            request.ContentType = "application/json";
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(msgToSend, 0, msgToSend.Length);
+            dataStream.Close();
+
+            WebResponse response = request.GetResponse();
+
+            using(dataStream = response.GetResponseStream())
+            {
+                DataContractJsonSerializer lRMSer = new DataContractJsonSerializer(typeof(LoginResponseMessage));
+                LoginResponseMessage lRM = (LoginResponseMessage)lRMSer.ReadObject(dataStream);
+                return lRM;
+            }
+        }
+
+        public LoginResponseMessage LoginToAccount(string name, string password)
+        {
+            WebRequest request = WebRequest.Create("http//localhost/api/login");
+            request.Credentials = CredentialCache.DefaultCredentials;
+            request.Method = "POST";
+
+            UserInformationMessage uIF = new UserInformationMessage();
+
+            uIF.Username = name;
+            uIF.Password = password;
+
+            MemoryStream mStream = new MemoryStream();
+            DataContractJsonSerializer dJsonSerializer = new DataContractJsonSerializer(typeof(UserInformationMessage));
+
+            dJsonSerializer.WriteObject(mStream, uIF);
+
+            mStream.Position = 0;
+            StreamReader sReader = new StreamReader(mStream);
+
+            string requestString = sReader.ReadToEnd();
+            byte[] msgToSend = System.Text.Encoding.ASCII.GetBytes(requestString);
+            request.ContentLength = msgToSend.Length;
+            request.ContentType = "application/json";
+            Stream dataStream = request.GetRequestStream();
+            dataStream.Write(msgToSend, 0, msgToSend.Length);
+            dataStream.Close();
+
+            WebResponse response = request.GetResponse();
+
+            using (dataStream = response.GetResponseStream())
+            {
+                DataContractJsonSerializer lRMSer = new DataContractJsonSerializer(typeof(LoginResponseMessage));
+                LoginResponseMessage lRM = (LoginResponseMessage)lRMSer.ReadObject(dataStream);
+                return lRM;
+            }
+        }
+
+        /* send highscore
+           host send
+           */
+        
         public void DoMagic()
         {
-
             //WebRequest request = WebRequest.Create("https://my-json-server.typicode.com/typicode/demo/comments");
             //request.Method = "POST";
 
