98780bdff028ee5d98c3bee943a2ae016607c3fe
Kahpya <nikihap@gmail.com>
2019-09-26 21:26:33 +0200

    delete unused using statments

--- WebServer/Controllers/CreateUserController.cs
+++ WebServer/Controllers/CreateUserController.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-using System.Runtime.Serialization.Json;
 using System.IO;
+using System.Runtime.Serialization.Json;
 using System.Text;
+using System.Web.Http;
 
 namespace WebServer.Controllers
 {
--- WebServer/Controllers/LoginController.cs
+++ WebServer/Controllers/LoginController.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Web.Http;
-using System.Runtime.Serialization.Json;
+
 
 namespace WebServer.Controllers
 {
